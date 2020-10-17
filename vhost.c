1463d30c36aec8b477357663a400613cfbaebd7b
Evgeny Yakovlev <eyakovlev3@gmail.com>
2020-10-17 20:17:26 +0200

    vhost: implement VHOST_USER_GET_VRING_BASE
    Signed-off-by: Evgeny Yakovlev <eyakovlev3@gmail.com>

--- vhost.c
+++ vhost.c
@@ -665,6 +665,27 @@ static int set_vring_base(struct vhost_dev* dev, struct vhost_user_message* msg,
     return 0;
 }
 
+static int get_vring_base(struct vhost_dev* dev, struct vhost_user_message* msg, int* fds, size_t nfds)
+{
+    if (msg->hdr.size < sizeof(msg->vring_state)) {
+        return -1;
+    }
+
+    if (msg->vring_state.index > dev->num_queues) {
+        return -1;
+    }
+
+    /* Sync avail base between vring and underlying virtqueue */
+    struct vring* vring = &dev->vrings[msg->vring_state.index];
+    vring->avail_base = vring->vq.last_seen_avail;
+
+    /* Strangely enough spec says that we should stop vring on GET_VRING_BASE */
+    vring_stop(vring);
+
+    msg->vring_state.num = vring->avail_base;
+    return 0;
+}
+
 static void handle_message(struct vhost_dev* dev, struct vhost_user_message* msg, int* fds, size_t nfds)
 {
     VHOST_VERIFY(dev);
@@ -683,7 +704,7 @@ static void handle_message(struct vhost_dev* dev, struct vhost_user_message* msg
         set_vring_num,  /* VHOST_USER_SET_VRING_NUM        */
         set_vring_addr, /* VHOST_USER_SET_VRING_ADDR       */
         set_vring_base, /* VHOST_USER_SET_VRING_BASE       */
-        NULL, /* VHOST_USER_GET_VRING_BASE       */
+        get_vring_base, /* VHOST_USER_GET_VRING_BASE       */
         set_vring_kick, /* VHOST_USER_SET_VRING_KICK       */
         set_vring_call, /* VHOST_USER_SET_VRING_CALL       */
         set_vring_err,  /* VHOST_USER_SET_VRING_ERR        */
