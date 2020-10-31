d9cb37637094b8268caba4bf7209f298c530cddd
Evgeny Yakovlev <eyakovlev3@gmail.com>
2020-10-31 19:02:46 +0100

    tools/server: support disk image files
    Signed-off-by: Evgeny Yakovlev <eyakovlev3@gmail.com>

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -4,7 +4,8 @@ set -e
 
 SERVER=$(realpath ${SERVER:-build-x86/tools/server/vhost-server})
 VHOST_SOCK=$(realpath ${VHOST_SOCK:-/tmp/vhost.sock})
+DISK_IMAGE=$(realpath $1)
 
 [ -S $VHOST_SOCK ] && rm -f $VHOST_SOCK
 
-$SERVER $VHOST_SOCK
+$SERVER $VHOST_SOCK $DISK_IMAGE
--- tools/server/server.c
+++ tools/server/server.c
@@ -7,24 +7,21 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 #include <vhost.h>
 #include <virtio/blk.h>
 
 #define DIE(fmt, ...) do { \
-    fprintf(stderr, fmt, ##__VA_ARGS__); \
-    fprintf(stderr, "\n"); \
+    fprintf(stderr, fmt "\n", ##__VA_ARGS__); \
     exit(EXIT_FAILURE); \
 } while (0);
 
-enum {
-    VBLK_TEST_DEV_SECTORS = 1024,
-    VBLK_TEST_DEV_BSIZE = 4096,
-};
+static int g_fd = -1;
 
 static void usage(void)
 {
-    fprintf(stderr, "vhost-server socket-path\n");
+    fprintf(stderr, "vhost-server socket-path disk-image\n");
 }
 
 static int handle_vring_event(struct virtio_dev* vdev, struct vring* vring)
@@ -36,23 +33,54 @@ static int handle_vring_event(struct virtio_dev* vdev, struct vring* vring)
 
 int main(int argc, char** argv)
 {
-    if (argc != 2) {
+    if (argc != 3) {
         usage();
         exit(EXIT_FAILURE);
     }
 
     int error = 0;
     const char* socket_path = argv[1];
+    const char* disk_image = argv[2];
 
     error = access(socket_path, F_OK);
     if (!error) {
         DIE("Socket path %s already exists, refusing to reuse", socket_path);
     }
 
+    error = access(disk_image, F_OK | R_OK);
+    if (error) {
+        DIE("Disk image path %s does not exist or is not readable", disk_image);
+    }
+
+    bool ro = false;
+    error = access(disk_image, W_OK);
+    if (error) {
+        fprintf(stdout, "Disk image %s is not writable - will user readonly device\n", disk_image);
+        ro = true;
+    }
+
+    g_fd = open(disk_image, O_SYNC | (ro ? O_RDONLY : O_RDWR));
+    if (g_fd < 0) {
+        DIE("Could not open disk image file %s", disk_image);
+    }
+
+    struct stat st;
+    error = fstat(g_fd, &st);
+    if (error) {
+        DIE("Could not fstat disk image %s", disk_image);
+    }
+
+    if (st.st_blocks == 0) {
+        DIE("Disk image %s has 0 blocks", disk_image);
+    }
+
+    uint32_t blocks = st.st_size / VIRTIO_BLK_SECTOR_SIZE;
+    fprintf(stdout, "Using disk image %s, %u blocks\n", disk_image, blocks);
+
     struct virtio_blk vblk;
-    vblk.total_sectors = VBLK_TEST_DEV_SECTORS;
-    vblk.block_size = VBLK_TEST_DEV_BSIZE;
-    vblk.readonly = false;
+    vblk.total_sectors = blocks;
+    vblk.block_size = VIRTIO_BLK_SECTOR_SIZE;
+    vblk.readonly = ro;
     vblk.writeback = false;
     error = virtio_blk_init(&vblk);
     if (error) {
@@ -72,5 +100,6 @@ int main(int argc, char** argv)
         }
     }
 
+    close(g_fd);
     return 0;
 }
